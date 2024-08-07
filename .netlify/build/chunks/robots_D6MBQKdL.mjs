8cc140c9a3d0f8f24d676a3fe6456098f0dc645f
Paul <paul@theapplegates.com>
2024-08-07 13:46:46 -0400

    fonts changed

--- .netlify/build/chunks/robots_D6MBQKdL.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/robots_DlGuI1Rt.mjs');
-
-export { page };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        "sans": ["Atkinson", ...defaultTheme.fontFamily.sans],
+        "sans": ["Inter", ...defaultTheme.fontFamily.sans],
       },
       typography: {
         DEFAULT: {
