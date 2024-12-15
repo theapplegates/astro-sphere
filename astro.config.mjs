9e36d03bb2015ad074414d7f315c66da441d84ec
Paul Applegate <paul@theapplegates.com>
2024-12-15 14:07:48 -0500

    v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sphere-demo.vercel.app",
+  site: "http://the-sphere.paulapplegate.com",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
