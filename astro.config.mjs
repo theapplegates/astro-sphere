418077a6ff027fc8d18396c62d574808431f9cd6
Paul Applegate <thor4@bobsyouruncle.local>
2025-02-16 18:37:37 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,14 @@ import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
-image: {
-  // Used for all `<Image />` and `<Picture />` components unless overridden
-  experimentalLayout: 'responsive',
-   },
-experimental: {
-  responsiveImages: true,
-},
 export default defineConfig({
   site: "https://the-sphere.paulapplegate.com",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  image: {
+    // Used for all <Image /> and <Picture /> components unless overridden
+    experimentalLayout: 'responsive',
+  },
+  experimental: {
+    responsiveImages: true,
+  },
 })
