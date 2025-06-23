4c52a6fb81d807a0da02007c441cc337486030f3
Paul <paul@theapplegates.com>
2025-06-23 16:57:49 -0400

    font change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        "sans": ["Atkinson", ...defaultTheme.fontFamily.sans],
+        "sans": ["wotfardregular", ...defaultTheme.fontFamily.sans],
       },
       typography: {
         DEFAULT: {
